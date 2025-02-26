e59953b409217b21d862c83a19bcec34de7b6490
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-02-26 13:12:54 +0000

    adding jokes data for Dark and Furturistic Jokes

---
+++ src/main/java/com/jokes/jokes_book/Models/DarkJokesData.java
@@ -0,0 +1,42 @@
+package com.jokes.jokes_book.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DarkJokesData {
+    private static List<String> darkJokes = new ArrayList<>();
+    static {
+        darkJokes.add("Why don’t graveyards ever get overcrowded? Because people are dying to get in!");
+        darkJokes.add("Why did the orphan bring a ladder to school? To reach their parent-teacher conference!");
+        darkJokes.add("Why don’t cannibals eat clowns? Because they taste funny!");
+        darkJokes.add("What’s the hardest part about playing chess with a zombie? Trying to stop them from eating the pieces!");
+        darkJokes.add("Why don’t skeletons fight each other? They don’t have the guts!");
+        darkJokes.add("Why did the man bring a knife to the party? Because it was a cutting-edge event!");
+        darkJokes.add("I told my wife I wanted to be cremated… She made an appointment for next Tuesday!");
+        darkJokes.add("Why don’t ghosts apply for jobs? Because they have no body to interview!");
+        darkJokes.add("Why did the scarecrow win an award? Because he was outstanding in his field… unlike his neighbor, who disappeared mysteriously!");
+        darkJokes.add("Why did the executioner break up with his girlfriend? He needed a clean cut!");
+        darkJokes.add("What’s the worst part about getting hit by a train? You can’t even complain about it afterward!");
+        darkJokes.add("Why don’t serial killers use social media? They already follow too many people!");
+        darkJokes.add("I got fired from my job at the cemetery… Apparently, I wasn’t doing a grave job!");
+        darkJokes.add("Why did the man bring a ladder to the bar? Because he heard the drinks were on the house!");
+        darkJokes.add("Why did the vampire break up with his girlfriend? Because she gave him too much space – and he needed blood!");
+        darkJokes.add("I have a joke about construction workers… But it’s still under development, just like that collapsed building!");
+        darkJokes.add("Why do hospitals have so many doors? Because patients tend to leave unexpectedly!");
+        darkJokes.add("Why did the doctor bring a scalpel to the dinner table? He wanted to make a quick incision!");
+        darkJokes.add("Why do graveyards have fences? Because people are dying to get out too!");
+        darkJokes.add("What did the undertaker say before closing the casket? 'That's a wrap!'");
+    }
+    private static List<String> category = new ArrayList<>();
+    static {
+        category.add("Dark Jokes");
+    }
+    private static List<String> author = new ArrayList<>();
+    static {
+        author.add("Lola");
+    }
+    private static List<String> tag = new ArrayList<>();
+    static {
+        tag.add("Dark");
+    }
+}
---
+++ src/main/java/com/jokes/jokes_book/Models/FuturisticJokesData.java
@@ -0,0 +1,39 @@
+package com.jokes.jokes_book.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuturisticJokesData {
+    private static List<String> futuristicJoke = new ArrayList<>();
+    static {
+        futuristicJoke.add("Why did the AI break up with its girlfriend? It needed more space!");
+        futuristicJoke.add("Why don’t robots ever get lost? Because they always follow the algorithm!");
+        futuristicJoke.add("What do you call a spaceship full of smart people? A brainstorm!");
+        futuristicJoke.add("Why did the self-driving car get pulled over? It couldn’t handle the traffic laws update!");
+        futuristicJoke.add("Why do time travelers make great friends? Because they’re always there for you, before you even need them!");
+        futuristicJoke.add("What’s a robot’s favorite type of music? Heavy metal!");
+        futuristicJoke.add("Why did the WiFi break up with the computer? Too many connections!");
+        futuristicJoke.add("Why did the astronaut bring a ladder to space? Because he wanted to reach new heights!");
+        futuristicJoke.add("What do you call an alien with three eyes? An a-liiien!");
+        futuristicJoke.add("Why did the quantum physicist refuse to tell a joke? Because it was both funny and not funny at the same time!");
+        futuristicJoke.add("How does a robot flirt? By saying, ‘You complete my circuits!’");
+        futuristicJoke.add("Why don’t aliens use social media? They don’t want to be followed!");
+        futuristicJoke.add("Why was the spaceship so calm? Because it had zero Gs!");
+        futuristicJoke.add("What do you call a planet that sings? Neptune-ted!");
+        futuristicJoke.add("What’s a robot’s favorite exercise? Circuit training!");
+        futuristicJoke.add("Why do aliens never argue? Because they always find common ground!");
+        futuristicJoke.add("What’s a hacker’s favorite snack? Cookies… but only if they’re enabled!");
+        futuristicJoke.add("Why don’t AI assistants ever get tired? Because they always run on caffeine… and code!");
+        futuristicJoke.add("What’s an astronaut’s favorite part of a computer? The space bar!");
+        futuristicJoke.add("Why did the scientist break up with the light bulb? Because it wasn’t bright enough for them!");
+    }
+    private static List<String> category = new ArrayList<>();
+    static {
+        category.add("Futuristic Jokes");
+    }
+    private static List<String> author = new ArrayList<>();
+    static {
+        author.add("Simon");
+    }
+
+}
